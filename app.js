1a0835e460c3c31aef983281460356c209b175c0
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-22 10:05:35 +0000

    added fetch and display repos

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-//Instantiate Classes
-const github = new GitHub;
+// Init Github
+const github = new Github;
+// Init UI
 const ui = new UI;
 
-//Search Input
+// Search input
 const searchUser = document.getElementById('searchUser');
 
-//Search Input event listener
+// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-  //Get input text
+  // Get input text
   const userText = e.target.value;
 
   if(userText !== ''){
-    //HTTP call
+    // Make http call
     github.getUser(userText)
       .then(data => {
         if(data.profile.message === 'Not Found') {
-          //Show alert
+          // Show alert
           ui.showAlert('User not found', 'alert alert-danger');
-          
         } else {
-          //Show profile
-          ui.showProfile(data.profile); 
+          // Show profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
   } else {
-    //Clear the profile
+    // Clear profile
     ui.clearProfile();
   }
 });
--- github.js
+++ github.js
@@ -1,15 +1,22 @@
-class GitHub {
+class Github {
   constructor() {
     this.client_id = '00721a573207f7595414';
     this.client_secret = 'b649fed86f850497c0c24f4ea9ba507a8de607f9';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}
-                            &client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
